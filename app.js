7f2adffdfc09c65ca5036b869c3d877bbe529138
Tim Anderson <tdanderson805@gmail.com>
2020-11-30 14:10:02 -0800

    refactor grocery list

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// TODO
+// // TODO
+var GroceryListItem = (props) => (
+  <ul>
+    <li>{props.groceryItems[0]}</li>
+    <li>{props.groceryItems[1]}</li>
+  </ul>
+);
+//what happens if the array and li count don't match up?
+
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryListItem groceryItems={['coffee', 'juice']}/>
+  </div>
+);
+
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
