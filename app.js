0da048f2471d43288c0633191173e4e09242a052
Tim Anderson <tdanderson805@gmail.com>
2020-11-30 16:53:00 -0800

    Assingment Complete

--- app.js
+++ app.js
@@ -1,19 +1,88 @@
 // // TODO
-var GroceryListItem = (props) => (
+
+var itemsToBuy = ['coffee', 'apple', 'juice']
+
+
+class GroceryListItem extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    // isMouseInside: false
+
+    this.state = {
+      hovering: false
+    };
+
+  }
+
+
+  mouseEnter () {
+    this.setState({ hovering: true });
+  }
+  mouseLeave () {
+    this.setState({ hovering: false });
+  }
+
+  render() {
+
+    var style = {
+      fontWeight: this.state.hovering ? 'bold' : 'normal'
+      // textDecoration: this.state.hovering ? 'line-through' : 'none'
+    }
+
+    
+    return (
+      <li style={style}
+      onMouseEnter={this.mouseEnter.bind(this)}
+      onMouseLeave={this.mouseLeave.bind(this)}
+      >
+        {this.props.groceryItems}
+      </li>
+    );
+  }
+}
+
+
+var GroceryList = (props) => (
   <ul>
-    <li>{props.groceryItems[0]}</li>
-    <li>{props.groceryItems[1]}</li>
+    {props.groceryItems.map(groceryItems =>
+    <GroceryListItem  groceryItems={groceryItems} />
+    )}
   </ul>
 );
-//what happens if the array and li count don't match up?
+
 
 var App = () => (
   <div>
     <h2>My Grocery List</h2>
-    <GroceryListItem groceryItems={['coffee', 'juice']}/>
+    <GroceryList groceryItems={itemsToBuy}/>
   </div>
 );
 
 
 
 ReactDOM.render(<App />, document.getElementById("app"));
+
+
+
+//working code from previous checkpoint
+// // // TODO
+// var GroceryListItem = (props) => (
+//   <ul>
+//     <li>{props.groceryItems[0]}</li>
+//     <li>{props.groceryItems[1]}</li>
+//   </ul>
+// );
+// //what happens if the array and li count don't match up?
+
+// var App = () => (
+//   <div>
+//     <h2>My Grocery List</h2>
+//     <GroceryListItem groceryItems={['coffee', 'juice']}/>
+//   </div>
+// );
+
+
+
+// ReactDOM.render(<App />, document.getElementById("app"));
